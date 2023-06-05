c7be132488fa4ae0bf9b6de68faa1a1f8eba72b3
francis-tg <franc9885@gmail.com>
2023-06-05 08:47:05 +0000

    fix fetch router

--- controllers/addRouter.js
+++ controllers/addRouter.js
@@ -1,12 +1,13 @@
-const db = require("../models")
+const db = require("../models");
 module.exports.addRouter = (req, res, next) => {
-    if (Object.keys(req.body).length > 0) {
-        const { host, user, password,name } = req.body
-        const newRouter = db.User.create({ host, user, password, name });
-        return res.status(201).json(newRouter);
-    }
-}
-module.exports.getRouter = (req, res, next) => {
-    const routers = db.User.findAll();
-    return res.status(200).json(routers);
-}
+  if (Object.keys(req.body).length > 0) {
+    const {ip, user, password, name} = req.body;
+    const newRouter = db.Router.create({host: ip, user, password, name});
+    return res.status(201).json(newRouter);
+  }
+};
+module.exports.getRouter = async (req, res, next) => {
+  const routers = await db.Router.findAll({raw: true});
+
+  return res.status(200).json(routers);
+};
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use("/router", require("./routes/router"));
 app.use("/router/interface", require("./routes/interface"));
 app.use("/router/bridge", require("./routes/bridge"));
